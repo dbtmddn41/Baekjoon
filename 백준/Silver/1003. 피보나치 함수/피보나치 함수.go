66248b1b7bf231226e11682a6510735e75ef19ae
dbtmddn41 <81400857+dbtmddn41@users.noreply.github.com>
2023-08-03 16:34:45 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 928 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.go
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.go
@@ -2,32 +2,36 @@ package main
 
 import "fmt"
 
+type ans_set struct {
+	zero int
+	one int
+}
 
-var known map[int] [2]int
+var known map[int] ans_set
 
 func main() {
 	var T int
 	var N int
-	known = make(map[int] [2]int)
-    known[0] = [2]int{1, 0}
-    known[1] = [2]int{0, 1}
-    known[2] = [2]int{1, 1}
+	known = make(map[int] ans_set)
+    known[0] = ans_set{1, 0}
+    known[1] = ans_set{0, 1}
+    known[2] = ans_set{1, 1}
 
 	fmt.Scanln(&T)
 	for j := 0; j < T; j++ {
 		fmt.Scanln(&N)
 		ans := fibbo(N)
-		fmt.Println(ans[0], ans[1])
+		fmt.Println(ans.zero, ans.one)
 	}
 }
 
-func fibbo(N int) [2]int {
+func fibbo(N int) ans_set {
 	if ans, ok := known[N]; ok {
         return ans
     }
 	return add(fibbo(N-2), fibbo(N-3))
 }
 
-func add(a [2]int, b [2]int) [2]int {
-	return [2]int{2*a[0]+b[0], 2*a[1]+b[1]}
+func add(a ans_set, b ans_set) ans_set {
+	return ans_set{2*a.zero+b.zero, 2*a.one+b.one}
 }
